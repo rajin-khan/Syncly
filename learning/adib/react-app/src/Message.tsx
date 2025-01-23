9910c224fb59d854b42bf46b639b4a3697cc8fbf
Rajin Khan <rajin.khan2001@gmail.com>
2025-01-23 18:51:27 +0600

    Learn how to create a basic React component

---
+++ learning/adib/react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+
+// PascalCasing for React components
+function Message() {
+    //Javascript XML being used below, not HTML
+    const name = 'Rajin';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+export default Message;
