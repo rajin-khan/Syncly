9f9f6fc03123072fdf219d5c9a9ba391f94a2871
Rajin Khan <rajin.khan2001@gmail.com>
2025-01-25 21:39:31 +0600

    Learn how to pass children to components

--- learning/adib/react-app/src/App.tsx
+++ learning/adib/react-app/src/App.tsx
@@ -1,15 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-    let items = ["New York", "San Francisco", "Toky", "London", "Paris"];
-
-    const handleSelectItem = (item: string) => {
-        console.log(item);
-    }
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+        <Alert>
+            Hello <span>World</span>
+        </Alert>
     </div>
   );
 }
