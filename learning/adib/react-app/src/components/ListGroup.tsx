3db42d0fba502aaeddc98bd3c2c9c2643cdb0a3b
Rajin Khan <rajin.khan2001@gmail.com>
2025-01-25 20:59:37 +0600

    Learn about event handling

--- learning/adib/react-app/src/components/ListGroup.tsx
+++ learning/adib/react-app/src/components/ListGroup.tsx
@@ -1,6 +1,10 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Toky", "London", "Paris"];
-  items = [];
+
+  //Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   //below, we learn how true && 1 = 1, or true && 'rajin' = 'rajin', and false && 'rajin' = false
   //this is often used in conditional rendering.
@@ -9,8 +13,14 @@ function ListGroup() {
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
