61660034a6995f0397bf89fca4ab90b9bc9ec1f7
0jayer <100233258+0jayer@users.noreply.github.com>
2025-02-01 01:00:59 +0600

    Update server.js
    Fixed the server link

--- learning/ojayer/Demo/server.js
+++ learning/ojayer/Demo/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const authRoutes = require('./auth');
 
-const app = express();
 
-app.use('/', authRoutes);
+const app = express();
+app.use(authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Express server!');
+});
 
 app.listen(5500, () => {
     console.log('Server running on http://localhost:5500');
+    console.log('Authenticate using the following link: http://localhost:5500/auth/google');
 });
