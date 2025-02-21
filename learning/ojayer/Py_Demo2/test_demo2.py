2c14daa5a901e3647328ea7d5b314256df9cefe6
0jayer <100233258+0jayer@users.noreply.github.com>
2025-02-22 00:22:51 +0600

    Update test_demo2.py

--- learning/ojayer/Py_Demo2/test_demo2.py
+++ learning/ojayer/Py_Demo2/test_demo2.py
@@ -1,14 +1,31 @@
-from google_drive import googleDrive # Import the GoogleDriveService class
+from DriveManager import DriveManager
+from google_drive import googleDrive
+from Dropbox import DropboxService
 
-# Step 1: Create an instance of GoogleDriveService
-gdrive = googleDrive()
+# Initialize DriveManager
+manager = DriveManager()
 
-# Step 2: Authenticate the service
-gdrive.authenticate(bucket_number=1)
+# Add Google Drive accounts
+manager.add_drive(googleDrive(), bucket_number=1)
+manager.add_drive(googleDrive(), bucket_number=2)
+manager.add_drive(googleDrive(), bucket_number=3)
 
-# Step 3: Check storage
-limit,storage = gdrive.check_storage()
+dropbox_service = DropboxService(app_key="w84emdpux17qpnj", app_secret="x6ce7dtmj51xqc7")
+manager.add_drive(dropbox_service, bucket_number=4)
 
-# Step 4: Print the results
-print(f"Storage Limit: {limit}")
-print(f"Storage Used: {storage}")
+ # Check storage for all services
+storages,limit,usage = manager.check_all_storages()
+ # Print results
+for storage in storages:
+    print(f"{storage['Provider']} - Drive {storage['Drive Number']}:")
+    print(f"  - Storage Limit: {storage['Storage Limit (bytes)']:.2f} GB")
+    print(f"  - Used Storage: {storage['Used Storage (bytes)']:.2f} GB")
+    print(f"  - Free Storage: {storage['Free Storage']:.2f} GB")
+    print(f"  - Provider: {storage['Provider']}")
+    print("-" * 30)
+# Print Total Storage
+print("Storage Details")
+print("-" * 30)
+print(f"Total Storage: {round(limit/1024**3,2)} GB")
+print(f"Used Space: {round(usage/1024**3,2)} GB")
+print(f"Free Space: {round((limit-usage)/1024**3,2)} GB")
