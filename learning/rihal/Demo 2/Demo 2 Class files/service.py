310a5d8051db79e1f61b35509e1e4790d8ad4cf4
Rihal Mahmood <quazi.mahmood@northsouth.edu>
2025-02-22 17:05:29 +0600

    Changes to service.py

---
+++ learning/rihal/Demo 2/Demo 2 Class files/service.py
@@ -0,0 +1,17 @@
+#import libraries for abstraction
+from abc import ABC, abstractmethod
+
+
+class service(ABC):
+    #abstract methods for service classes
+    @abstractmethod
+    def authenticate(self, bucket_numer: int):
+        pass
+
+    @abstractmethod
+    def listFiles(self, max_results: int = None, query: str = None):
+        pass
+
+    @abstractmethod
+    def check_storage(self):
+        pass
