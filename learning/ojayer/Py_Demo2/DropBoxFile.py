bb6fc95fe727b83442e58c82475ccbcbf27ad70e
0jayer <100233258+0jayer@users.noreply.github.com>
2025-02-23 10:32:09 +0600

    Final Update DropBoxFile.py

--- learning/ojayer/Py_Demo2/DropBoxFile.py
+++ learning/ojayer/Py_Demo2/DropBoxFile.py
@@ -5,6 +5,7 @@ import re
 import logging
 from dropbox.exceptions import AuthError, ApiError
 from dropbox.files import WriteMode
+from FileHandler import FileHandler
 from DriveManager import DriveManager
 
 # Constants
@@ -37,7 +38,7 @@ class DropBoxFile:
             # Upload the entire file
             with open(file_path, "rb") as f:
                 best_bucket[1].client.files_upload(f.read(), f"/{file_name}", mode=WriteMode("overwrite"))
-            metadata["chunks"].append({"chunk_name": file_name, "account": best_bucket[2]+1})
+            metadata["chunks"].append({"chunk_name": file_name, "account": f"{best_bucket[2]+1}"})
         else:
             # Split the file into chunks and upload
             offset = 0
