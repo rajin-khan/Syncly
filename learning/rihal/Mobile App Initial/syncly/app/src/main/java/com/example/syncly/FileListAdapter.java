a601dee6e18a028ce454c1ecde7e643ab2ba1021
Rihal Mahmood <quazi.mahmood@northsouth.edu>
2025-03-23 03:23:03 +0600

    Redesigned View_Files

--- learning/rihal/Mobile App Initial/syncly/app/src/main/java/com/example/syncly/FileListAdapter.java
+++ learning/rihal/Mobile App Initial/syncly/app/src/main/java/com/example/syncly/FileListAdapter.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 import java.util.Map;
@@ -23,13 +26,14 @@ public class FileListAdapter extends RecyclerView.Adapter<FileListAdapter.ViewHo
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView fileName;
-        TextView fileProvider;
+        TextView fileName, fileProvider;
+        ImageView fileThumbnail;
 
         public ViewHolder(View view) {
             super(view);
             fileName = view.findViewById(R.id.file_name);
             fileProvider = view.findViewById(R.id.file_provider);
+            fileThumbnail = view.findViewById(R.id.file_thumbnail);
         }
     }
 
@@ -45,6 +49,17 @@ public class FileListAdapter extends RecyclerView.Adapter<FileListAdapter.ViewHo
         Map<String, String> file = fileList.get(position);
         holder.fileName.setText(file.get("name"));
         holder.fileProvider.setText(file.get("provider"));
+
+        // For now, use a placeholder thumbnail
+        String name = file.get("name");
+        assert name != null;
+        if (name.endsWith(".pdf")) {
+            Glide.with(context).load(R.drawable.ic_pdf_foreground).into(holder.fileThumbnail);
+        } else if (name.endsWith(".jpg") || name.endsWith(".png")) {
+            Glide.with(context).load(R.drawable.ic_jpg_foreground).into(holder.fileThumbnail);
+        } else {
+            Glide.with(context).load(R.drawable.ic_file_placeholder_foreground).into(holder.fileThumbnail);
+        }
     }
 
     @Override
